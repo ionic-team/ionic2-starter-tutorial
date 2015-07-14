42c4afecbc7a352c58a21d4f6284c1ba3dc9d050
Tim Lancina <tim@drifty.com>
2015-07-14 14:51:36 -0500

    fix(SongDetailPage): remove Button component

--- js/pages/song-detail.js
+++ js/pages/song-detail.js
@@ -1,10 +1,10 @@
 import {bootstrap, NgFor, ComponentAnnotation as Component,
   DirectiveAnnotation as Directive, ViewAnnotation as View} from 'angular2/angular2';
-import {NavParams, NavController, Navbar, NavbarTemplate, Content, Button} from 'ionic/ionic';
+import {NavParams, NavController, Navbar, NavbarTemplate, Content} from 'ionic/ionic';
 
 @Component({ selector: 'ion-view' })
 @View({
-  directives: [Content, Button, Navbar, NavbarTemplate],
+  directives: [Content, Navbar, NavbarTemplate],
   templateUrl: 'templates/pages/song_detail.html'
 })
 export class SongDetailPage {
