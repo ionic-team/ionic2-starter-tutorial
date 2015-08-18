3101309e482bb4e3797d8a5cd701dd6029ac22e5
Tim Lancina <tim@ionic.io>
2015-08-18 15:59:56 -0500

    add sass building

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,15 @@ var gulp = require('gulp'),
     webpack = require('webpack'),
     del = require('del'),
     minimist = require('minimist'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
     connect = require('gulp-connect');
 
-// Serve config
 var flagConfig = {
   string: 'port',
   default: { port: 8100 }
 };
 var flags = minimist(process.argv.slice(2), flagConfig);
-
 
 // Tasks
 /******************************************************************************/
@@ -31,8 +31,41 @@ gulp.task('build', function(done) {
   compile(false, done);
 });
 
-gulp.task('watch', ['serve'], function(done) {
+gulp.task('watch', ['serve', 'sass', 'fonts'], function(done) {
+  gulp.watch('www/app/**/*.scss', ['sass']);
   compile(true, done);
+});
+
+gulp.task('sass', function(){
+  var autoprefixerOpts = {
+    browsers: [
+      'last 2 versions',
+      'iOS >= 7',
+      'Android >= 4',
+      'Explorer >= 10',
+      'ExplorerMobile >= 11'
+    ],
+    cascade: false
+  };
+
+  return gulp.src('www/app/app.scss')
+    .pipe(sass({
+      includePaths: ['node_modules/ionic-framework/src/scss'],
+    }))
+    .on('error', function(err){
+      console.error(err.message);
+      this.emit('end');
+    })
+    .pipe(autoprefixer(autoprefixerOpts))
+    .pipe(gulp.dest('www/build/css'));
+})
+
+gulp.task('fonts', function() {
+  return gulp.src([
+      'node_modules/ionic-framework/fonts/**/*.ttf',
+      'node_modules/ionic-framework/fonts/**/*.woff'
+    ])
+    .pipe(gulp.dest('www/build/fonts'));
 });
 
 /******************************************************************************/
