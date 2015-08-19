0cca25a1dbe362f0a921bb3638df5be45643ba1b
Adam Bradley <adambradley25@gmail.com>
2015-08-18 21:39:53 -0500

    starter comments

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,48 @@
+/**********************
+ * Gulpfile
+ * Be sure to run `npm install` for `gulp` and its
+ * following tasks to be available from the command line.
+ **********************/
+
+// node module imports
 var gulp = require('gulp'),
     webpack = require('webpack'),
-    del = require('del'),
     minimist = require('minimist'),
     sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     watch = require('gulp-watch'),
     connect = require('gulp-connect');
 
-var flagConfig = {
-  string: 'port',
-  default: { port: 8100 }
-};
-var flags = minimist(process.argv.slice(2), flagConfig);
 
-// Tasks
-/******************************************************************************/
 
-gulp.task('clean', function(done) {
-  del(['www/build'], done);
+/******************************
+ * gulp watch
+ * Builds the app, and will rebuild the app when any
+ * of its files change. Additionally, a local web server
+ * will be started so you can view the built app.
+ ******************************/
+gulp.task('watch', ['serve', 'sass', 'fonts'], function(done) {
+  watch('www/app/**/*.scss', function(){
+    gulp.start('sass');
+  });
+  compile(true, done);
 });
 
+
+/******************************
+ * gulp build
+ * Builds the app from its source files.
+ ******************************/
+gulp.task('build', function(done) {
+  compile(false, done);
+});
+
+
+/******************************
+ * gulp serve
+ * Starts a local web server so the app can be
+ * viewed from http://localhost:8100/
+ ******************************/
 gulp.task('serve', function() {
   connect.server({
     root: 'www',
@@ -28,17 +51,14 @@ gulp.task('serve', function() {
   });
 });
 
-gulp.task('build', function(done) {
-  compile(false, done);
-});
 
-gulp.task('watch', ['serve', 'sass', 'fonts'], function(done) {
-  watch('www/app/**/*.scss', function(){
-    gulp.start('sass');
-  });
-  compile(true, done);
-});
-
+/******************************
+ * gulp sass
+ * Used to convert Sass files into one bundled CSS file
+ * which browsers can use. Additionally, this task uses
+ * auto-prefixer, which will automatically add required
+ * CSS prefixes when needed.
+ ******************************/
 gulp.task('sass', function(){
   var autoprefixerOpts = {
     browsers: [
@@ -61,8 +81,13 @@ gulp.task('sass', function(){
     })
     .pipe(autoprefixer(autoprefixerOpts))
     .pipe(gulp.dest('www/build/css'));
-})
+});
 
+
+/******************************
+ * gulp fonts
+ * Used to copy ionic font files to your build directory.
+ ******************************/
 gulp.task('fonts', function() {
   return gulp.src([
       'node_modules/ionic-framework/fonts/**/*.ttf',
@@ -71,7 +96,22 @@ gulp.task('fonts', function() {
     .pipe(gulp.dest('www/build/fonts'));
 });
 
-/******************************************************************************/
+
+/******************************
+ * gulp clean
+ * Used to delete previous build files to ensure
+ * the next build has the most recent files.
+ ******************************/
+gulp.task('clean', function(done) {
+  var del = require('del');
+  del(['www/build'], done);
+});
+
+
+
+/**********************
+ * Compile
+ **********************/
 
 function compile(watch, cb) {
   // prevent gulp calling done callback more than once when watching
@@ -82,15 +122,15 @@ function compile(watch, cb) {
 
   // https://github.com/webpack/docs/wiki/node.js-api#statstojsonoptions
   var statsOptions = {
-    "colors": true,
-    "modules":true,
-    "chunks":false,
-    "exclude":["node_modules"]
+    'colors': true,
+    'modules': true,
+    'chunks': false,
+    'exclude': ['node_modules']
   }
 
   // Can either run (one time compile) or watch
   // https://github.com/webpack/docs/wiki/node.js-api
-  var compilerFunc = watch ? "watch" : "run";
+  var compilerFunc = (watch ? 'watch' : 'run');
   var compilerFuncArgs = [compileHandler];
   watch && compilerFuncArgs.unshift(null); // watch takes config obj as first arg
 
@@ -106,3 +146,11 @@ function compile(watch, cb) {
     }
   }
 }
+
+
+// command line flag config
+var flagConfig = {
+  string: 'port',
+  default: { port: 8100 }
+};
+var flags = minimist(process.argv.slice(2), flagConfig);
