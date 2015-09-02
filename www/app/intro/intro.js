e6851b7451de13446ead824fb8742dbe0020babe
Max Lynch <max@drifty.com>
2015-09-02 16:49:16 -0500

    Updated intro

--- www/app/intro/intro.js
+++ www/app/intro/intro.js
@@ -7,7 +7,62 @@ import {IonicApp, IonicView, NavController, Slides} from 'ionic/ionic';
 })
 export class IntroPage {
   constructor(app: IonicApp, nav: NavController, @ViewQuery(Slides) public slides: QueryList<Slides>) {
+    this.nextButton = 'Next';
+    this.prevButton = 'Skip';
+
+    this.nav = nav;
+
+    // Get the child slider.
+    // Note: this will be getting cleaner in a future Angular 2 release
     slides.onChange((c) => {
+      this.mySlider = slides.first;
+      console.log('Found slider', this.mySlider);
     });
+  }
+
+  go() {
+    // You might want to navigate here, the tutorial is done.
+    // ex: this.nav.push(HomePage)
+  }
+
+  next() {
+    if(!this.mySlider) { return; }
+
+    if(this.mySlider.isAtEnd()) {
+      alert('Done!');
+      this.go();
+    } else {
+      this.mySlider.next();
+      this.updateButtons();
+    }
+  }
+
+  prev() {
+    if(!this.mySlider) { return; }
+
+    if(this.mySlider.getIndex() == 0) {
+      alert('Skipped tutorial');
+      this.go();
+    } else {
+      this.mySlider.prev();
+      this.updateButtons();
+    }
+  }
+
+  // Update the header buttonses text
+  updateButtons() {
+    if(!this.mySlider) { return; }
+
+    if(this.mySlider.isAtEnd()) {
+      this.nextButton = 'End';
+    } else {
+      this.nextButton = 'Next';
+    }
+
+    if(this.mySlider.getIndex() > 0) {
+      this.prevButton = 'Back'
+    } else {
+      this.prevButton = 'Skip'
+    }
   }
 }
