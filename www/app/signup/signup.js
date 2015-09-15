c4b28b0a7488e7b8ff39a07f26f0c55103a8a076
Max Lynch <ihasmax@gmail.com>
2015-09-15 12:15:50 -0500

    Login and signup improvements

--- www/app/signup/signup.js
+++ www/app/signup/signup.js
@@ -1,6 +1,7 @@
 import {Validators, Control, ControlGroup} from 'angular2/forms';
 import {IonicApp, IonicView, NavController} from 'ionic/ionic';
 
+import {LoginPage} from '../login/login';
 
 @IonicView({
   templateUrl: 'app/signup/signup.html'
@@ -13,10 +14,12 @@ export class SignupPage {
       password: new Control("", Validators.required),
     });
 
+    this.loginPage = LoginPage;
   }
 
   doSignup(event) {
     // Handle the signup here:
+    console.log('Doing signup', this.form.value);
 
 
     // Don't allow the form to submit normally, since we
