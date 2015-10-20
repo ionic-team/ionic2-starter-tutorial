f372c5d33113288a1b91afe003d458d692a12979
Josh Bavari <josh@ionic.io>
2015-10-20 15:42:08 -0600

    Chore(webpack.config): Update webpack.config to work on both windows and mac

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,9 @@
+var path = require('path');
+var wwwPath = path.resolve(__dirname, 'www');
+var outputPath = path.join(wwwPath, 'build', 'js');
+var appPath = path.join(wwwPath, 'app');
+var appJsPath = path.join(appPath, 'app.js');
+
 module.exports = {
   entry: [
     "es6-shim",
@@ -6,11 +12,12 @@ module.exports = {
     "angular2/angular2",
     "ionic/ionic",
     "web-animations.min",
-    "./www/app/app.js" // your app entry
+    // "./www/app/app.js"
+    appJsPath
   ],
   output: {
-    path: __dirname + "/www",
-    filename: "build/js/app.bundle.js",
+    path: outputPath,
+    filename: 'app.bundle.js'
     //pathinfo: true // show module paths in the bundle, handy for debugging
   },
   module: {
@@ -18,13 +25,15 @@ module.exports = {
       {
         test: /\.js$/,
         loader: "awesome-typescript-loader?doTypeCheck=false&useBabel=true&useWebpackText=true",
-        include: /www\/app\//,
+        include: [wwwPath],
+        // include: /www(\/|\\)app(\/|\\)/,
         exclude: /node_modules/
       },
       {
         test: /\.ts$/,
         loader: "awesome-typescript-loader",
-        include: /www\/app\//,
+        // include: /www(\/|\\)app(\/|\\)/,
+        include: [wwwPath],
         exclude: /node_modules/
        }
     ]
@@ -34,11 +43,12 @@ module.exports = {
       "node_modules",
       "node_modules/ionic-framework/src/es5/common", // ionic-framework npm package (stable)
       "node_modules/ionic-framework/node_modules", // angular is a dependency of ionic
-      "node_modules/ionic-framework/js" // for web-animations polyfill
+      "node_modules/ionic-framework/js", // for web-animations polyfill
 
-     // "node_modules/ionic2/dist/src/es5/common", // driftyco/ionic2 repo (master)
-     // "node_modules/ionic2/node_modules",
-     // "node_modules/ionic2/dist/js"
+      // "node_modules/ionic2/dist/src/es5/common", // driftyco/ionic2 repo (master)
+      // "dist/src/es5/common",
+      // "node_modules/ionic2/node_modules",
+      // "node_modules/ionic2/dist/js"
     ],
     extensions: ["", ".js", ".ts"]
   }
