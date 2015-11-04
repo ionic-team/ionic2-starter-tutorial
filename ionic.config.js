b27bf5c95b16930a0a11474979eee5f590f2c792
jbavari <josh@drifty.com>
2015-11-04 14:04:08 -0700

    Revert "Remove files that are provided by app-base"
    This reverts commit 80b1e9afc54d35b5d93b8f911a26d22a9251ef55.

---
+++ ionic.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "name": "ionic2-starter-tutorial",
+  "proxies": null,
+  "fontsOutputPath": "www/build/fonts",
+  "sassEntryFile": "/www/app/app.scss",
+  "sassOutputFile": "/www/build/css/app.css",
+  "sassIncludePaths": [
+    "node_modules/ionic-framework/dist/src/scss"
+  ],
+  "sassWatchPattern": [
+    "www/**/*.scss"
+  ],
+  "htmlWatchPattern": [
+    "www/**/*.html"
+  ]
+}
