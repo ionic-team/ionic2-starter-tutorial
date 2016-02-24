703f58e2f29fa849518244d038f19f48cc4468e1
Tim Lancina <tim@ionic.io>
2016-02-23 19:19:19 -0600

    chore(): remove unused constructor

--- app/pages/hello-ionic/hello-ionic.ts
+++ app/pages/hello-ionic/hello-ionic.ts
@@ -4,8 +4,4 @@ import {Page} from 'ionic-framework/ionic';
 @Page({
   templateUrl: 'build/pages/hello-ionic/hello-ionic.html'
 })
-export class HelloIonicPage {
-  constructor() {
-
-  }
-}
+export class HelloIonicPage {}
