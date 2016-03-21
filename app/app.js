6067561aff125c536aacebaac632c77d09b5ec53
Tim Lancina <tim@ionic.io>
2016-03-21 16:53:26 -0500

    chore(): use ionic-native StatusBar

--- app/app.js
+++ app/app.js
@@ -1,4 +1,5 @@
 import {App, IonicApp, Platform, MenuController} from 'ionic-angular';
+import {StatusBar} from 'ionic-native';
 import {HelloIonicPage} from './pages/hello-ionic/hello-ionic';
 import {ListPage} from './pages/list/list';
 
@@ -31,21 +32,9 @@ class MyApp {
 
   initializeApp() {
     this.platform.ready().then(() => {
-      // The platform is now ready. Note: if this callback fails to fire, follow
-      // the Troubleshooting guide for a number of possible solutions:
-      //
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      //
-      // First, let's hide the keyboard accessory bar (only works natively) since
-      // that's a better default:
-      //
-      //
-      // For example, we might change the StatusBar color. This one below is
-      // good for light backgrounds and dark text;
-      if (window.StatusBar) {
-        window.StatusBar.styleDefault();
-      }
+      StatusBar.styleDefault();
     });
   }
 
