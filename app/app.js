d417fde9bf5c749df7fc222ede76640bacad83bb
Brandy Carney <brandyscarney@gmail.com>
2016-05-19 12:48:46 -0400

    fix(app): remove getComponent and fix template warnings

--- app/app.js
+++ app/app.js
@@ -1,4 +1,5 @@
-import {App, IonicApp, Platform, MenuController} from 'ionic-angular';
+import {ViewChild} from '@angular/core';
+import {App, Platform, MenuController} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
 import {HelloIonicPage} from './pages/hello-ionic/hello-ionic';
 import {ListPage} from './pages/list/list';
@@ -6,16 +7,17 @@ import {ListPage} from './pages/list/list';
 
 @App({
   templateUrl: 'build/app.html',
-  config: {} // http://ionicframework.com/docs/v2/api/config/Config/
+  config: {}, // http://ionicframework.com/docs/v2/api/config/Config/
+  queries: {
+    nav: new ViewChild('content')
+  }
 })
 class MyApp {
   static get parameters() {
-    return [[IonicApp], [Platform], [MenuController]];
+    return [[Platform], [MenuController]];
   }
 
-  constructor(app, platform, menu) {
-    // set up our app
-    this.app = app;
+  constructor(platform, menu) {
     this.platform = platform;
     this.menu = menu;
     this.initializeApp();
@@ -42,7 +44,6 @@ class MyApp {
     // close the menu when clicking a link from the menu
     this.menu.close();
     // navigate to the new page if it is not the current page
-    let nav = this.app.getComponent('nav');
-    nav.setRoot(page.component);
+    this.nav.setRoot(page.component);
   }
 }
