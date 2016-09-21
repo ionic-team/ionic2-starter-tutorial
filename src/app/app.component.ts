173daffaf0a97202f079c609884a2c34b8486cc7
Brandy Carney <brandyscarney@gmail.com>
2016-09-20 23:11:55 -0400

    refactor(app): refactor for beta.12

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
-import {Component, ViewChild} from '@angular/core';
-import {ionicBootstrap, Platform, MenuController, Nav} from 'ionic-angular';
-import {StatusBar} from 'ionic-native';
-import {HelloIonicPage} from './pages/hello-ionic/hello-ionic';
-import {ListPage} from './pages/list/list';
+import { Component, ViewChild } from '@angular/core';
+
+import { ionicBootstrap, Platform, MenuController, Nav } from 'ionic-angular';
+
+import { StatusBar } from 'ionic-native';
+
+import { HelloIonicPage } from './pages/hello-ionic/hello-ionic';
+import { ListPage } from './pages/list/list';
 
 
 @Component({
-  templateUrl: 'build/app.html'
+  templateUrl: 'app.html'
 })
 class MyApp {
   @ViewChild(Nav) nav: Nav;
@@ -32,7 +35,7 @@ class MyApp {
     this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      StatusBar.styleDefault();
+      // StatusBar.styleDefault();
     });
   }
 
@@ -43,5 +46,3 @@ class MyApp {
     this.nav.setRoot(page.component);
   }
 }
-
-ionicBootstrap(MyApp);
--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,17 +1,19 @@
-import {Component} from '@angular/core';
-import {NavController, NavParams} from 'ionic-angular';
-import {ItemDetailsPage} from '../item-details/item-details';
+import { Component } from '@angular/core';
+
+import { NavController, NavParams } from 'ionic-angular';
+
+import { ItemDetailsPage } from '../item-details/item-details';
 
 
 @Component({
-  templateUrl: 'build/pages/list/list.html'
+  templateUrl: 'list.html'
 })
 export class ListPage {
   selectedItem: any;
   icons: string[];
   items: Array<{title: string, note: string, icon: string}>;
 
-  constructor(public navCtrl: NavController, navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
 
