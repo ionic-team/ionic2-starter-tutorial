eaa4f5aa5d849a4271584490de63cee7cb6d2201
Justin Willis <jgw9617@gmail.com>
2016-09-26 15:16:21 -0500

    chore(): uncomment ionic-native

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -35,7 +35,7 @@ export class MyApp {
     this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      // StatusBar.styleDefault();
+      StatusBar.styleDefault();
     });
   }
 
