01c82554f2e8419725aee354008b6a1e59ab1eff
Spas Kyuchukov <lolotobg@users.noreply.github.com>
2016-12-23 11:51:45 +0200

    Remove selectedItem from list.ts

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -10,14 +10,10 @@ import { ItemDetailsPage } from '../item-details/item-details';
   templateUrl: 'list.html'
 })
 export class ListPage {
-  selectedItem: any;
   icons: string[];
   items: Array<{title: string, note: string, icon: string}>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
-
     this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
     'american-football', 'boat', 'bluetooth', 'build'];
 
