6ff869ba9da5a4b8425d74fbd7d85975ddc37933
mhartington <mikehartington@gmail.com>
2017-04-06 15:57:56 -0600

    fix(): fix page loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@ export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
   // make HelloIonicPage the root (or first) page
-  rootPage = 'HelloIonicPage';
+  rootPage = HelloIonicPage;
   pages: Array<{title: string, component: any}>;
 
   constructor(
